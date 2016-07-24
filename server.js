d59f56c5b71e5b88bd468d2418e6d002c1e3a5f9
Piyush Santwani <psantwani@gmail.com>
2016-07-25 02:55:51 +0530

    cross origin

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ app.listen(port, function(){
 	console.log("Listening at port " + port);
 });
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/go', function(req, res){
 	var start_lat = req.body.latitude;
 	var start_long = req.body.longitude;
